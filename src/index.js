6fd519a8cd277612f321e410c715119cfdd40af8
Danomanic <daniel.pomfret@googlemail.com>
2022-06-27 22:12:51 +0100

    First draft of website.

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import { engine } from 'express-handlebars';
+import ethels from './data/ethels.json' assert { type : "json" };
+
+const app = express();
+const expressPort = 3000;
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './src/views');
+app.use('/', express.static('./src/public'))
+
+app.get('/', (req, res) => {
+    res.render('home', { ethels: ethels.Ethels });
+});
+
+app.get('/list', (req, res) => {
+    res.render('list', { title: 'List of Ethels', ethels: ethels.Ethels });
+});
+
+app.get('/map', (req, res) => {
+    res.render('map', { title: 'Ethel Map', ethels: ethels.Ethels });
+});
+
+app.get('/ethel/:slug', (req, res) => {
+    var ethel = ethels.Ethels.find(o => o.slug === req.params.slug);
+    console.log(ethel);
+    res.render('ethel', { title: ethel.name, ethel: ethel });
+});
+
+app.listen(expressPort);
+console.log(`Server running on port ${expressPort}`);
