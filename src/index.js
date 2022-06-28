b46ccec0dd8b94902daa7e548c59c7e5140efe9a
Danomanic <daniel.pomfret@googlemail.com>
2022-06-28 12:13:49 +0100

    Add images and make look nice.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { engine } = require('express-handlebars');
+const compression = require('compression');
 const ethels = require('./data/ethels.json');
 
 const app = express();
@@ -8,23 +9,24 @@ const expressPort = process.env.PORT || 3000;
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set('views', './src/views');
-app.use('/', express.static('./src/public'))
+app.use('/', express.static('./src/public'));
+app.use(compression());
 
 app.get('/', (req, res) => {
-    res.render('home', { ethels: ethels.Ethels });
+  res.render('home', { ethels: ethels.Ethels });
 });
 
 app.get('/list', (req, res) => {
-    res.render('list', { title: 'List of Ethels', ethels: ethels.Ethels });
+  res.render('list', { title: 'List of Ethels', ethels: ethels.Ethels });
 });
 
 app.get('/map', (req, res) => {
-    res.render('map', { title: 'Ethel Map', ethels: ethels.Ethels });
+  res.render('map', { title: 'Ethel Map', ethels: ethels.Ethels });
 });
 
 app.get('/ethel/:slug', (req, res) => {
-    var ethel = ethels.Ethels.find(o => o.slug === req.params.slug);
-    res.render('ethel', { title: ethel.name, ethel: ethel });
+  const ethel = ethels.Ethels.find((o) => o.slug === req.params.slug);
+  res.render('ethel', { title: ethel.name, ethel });
 });
 
 app.listen(expressPort);
